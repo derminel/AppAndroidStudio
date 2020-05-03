0088df4b574d00cd231ff73ce12f9d90e7a3f8c4
chaverstraet <charlotte.student@student.uclouvain.be>
2020-05-03 17:29:59 +0200

    Profile done (but bug)

--- app/src/main/java/com/example/secondtest/PageProfilUpdate.java
+++ app/src/main/java/com/example/secondtest/PageProfilUpdate.java
@@ -14,6 +14,7 @@ public class PageProfilUpdate extends AppCompatActivity {
     EditText last_name;
     EditText address;
     EditText preferences;
+    ProfileDAO profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +22,26 @@ public class PageProfilUpdate extends AppCompatActivity {
         setContentView(R.layout.activity_page_profil_update);
 
         this.myDb = new DatabaseHelper(this);
+        this.profile = new ProfileDAO(this);
 
         this.name = findViewById(R.id.name_update);
         this.last_name = findViewById(R.id.last_name_update);
         this.address = findViewById(R.id.address_update);
         this.preferences = findViewById(R.id.preferences_update);
-
+        configureButtonSaveProfile();
 
     }
+
+    private void configureButtonSaveProfile() {
+        profile.updateProfile("derminel", name.toString(), last_name.toString(), address.toString(), preferences.toString());
+        Button buttonSaveProfile = (Button) findViewById(R.id.buttonSaveProfile);
+        buttonSaveProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                startActivity(new Intent(PageProfilUpdate.this, PageProfil.class));
+            }
+        });
+    }
+
 
 
 }
