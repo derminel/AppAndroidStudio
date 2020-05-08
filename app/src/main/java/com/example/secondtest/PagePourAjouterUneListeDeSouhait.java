dd3c7d992f6022eb7724ad84a2703ff30e56bc98
aurelarue <aurelien.larue@live.be>
2020-05-08 19:12:55 +0200

    encore un peu d'intent et de clean

--- app/src/main/java/com/example/secondtest/PagePourAjouterUneListeDeSouhait.java
+++ app/src/main/java/com/example/secondtest/PagePourAjouterUneListeDeSouhait.java
@@ -17,72 +17,67 @@ import java.util.ArrayList;
 
 public class PagePourAjouterUneListeDeSouhait extends AppCompatActivity {
 
-    private WishListDAO wishListDAO ;//
-    private boolean publicAccess;//
-    private EditText name;//
+    private WishListDAO wishListDAO ;
+    private boolean publicAccess;
+    private EditText name;
     private EditText recipient;
     private EditText description;
-    private Button Back;
     private String login;
 
     //creation de la page pour ajouter une liste de souhait
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_pour_ajouter_une_liste_de_souhait);
-
+        this.login = getIntent().getStringExtra("Login");
         this.wishListDAO = new WishListDAO(this);
         this.publicAccess = false ;
+
+        setContentView(R.layout.activity_page_pour_ajouter_une_liste_de_souhait);
         this.name = findViewById(R.id.setNameNewWishlist);
         this.recipient = findViewById(R.id.setRecipientNewWishlist);
         this.description = findViewById(R.id.setDescriptionNewWishlist);
-        this.login = getIntent().getStringExtra("Login");
 
+        configureconfirm();
+        configureback();
+        configureswitch();
+    }
+    private void start(Class<?> cls){
+        Intent page = new Intent(PagePourAjouterUneListeDeSouhait.this, cls);
+        page.putExtra("Login", login);
+        startActivity(page);
+    }
+    private void configureswitch(){
         Switch switchButton = findViewById(R.id.switchAccess);
-        Button buttonConfirm = findViewById(R.id.ConfirmAddWishlist);
-        Back = (Button)findViewById(R.id.GoBackAddWishlist);
-        Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activityretour();
-            }
-        });
-
         switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    publicAccess = true;
-                }
-                else {
-                    publicAccess = false;
-                }
-            }
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) { publicAccess = isChecked; }
         });
-
+    }
+    private void configureconfirm(){
+        Button buttonConfirm = findViewById(R.id.ConfirmAddWishlist);
         buttonConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (name.getText().toString().equals("") || recipient.getText().toString().equals("")){
-                    showToast("Please fill in the 2 first fields");
-                }
-                else{
+                if (name.getText().toString().equals("")){
+                    showToast("Please fill the name in");
+                } else{
                     WishList newList = configureWishList();
                     wishListDAO.addList(newList.getName(), publicAccess, newList.getListNb(), description.getText().toString(), recipient.getText().toString(), login);
                     showToast("WishList has been created");
-                    Intent intent = new Intent(PagePourAjouterUneListeDeSouhait.this, PagesListesDeSouhaits.class);
-                    intent.putExtra("Login", login);
-                    startActivity(intent);
+                    start(PagesListesDeSouhaits.class);
                 }
             }
         });
     }
-    public void activityretour(){
-        Intent intent = new Intent(this, PagesListesDeSouhaits.class);
-        intent.putExtra("Login", login);
-        startActivity(intent);
+    private void configureback(){
+        Button Back = findViewById(R.id.GoBackAddWishlist);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start(PagesListesDeSouhaits.class);
+            }
+        });
     }
-
 
     private WishList configureWishList(){
         return new WishList(this.name.getText().toString(), this.publicAccess,this);
